0ff5300ac98350b2606a5c1ceedf6ce39fc23a10
simplyking-decadev <56389903+simplyking-decadev@users.noreply.github.com>
2019-11-02 22:26:06 +0100

    set up function to save mail messages on local storage

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+function sendMailOnLocalStorage(message){
+    //check if their is an old mail else initialize 
+    var oldMail= JSON.parse(localStorage.getItem('Message')) || [];
+
+    const {email,subject,message_body}=message;
+
+    newMail={
+        email:email,
+        subject:subject,
+        message_body:message_body
+    }
+
+    if(oldMail){
+        oldMail.push(newMail)
+        localStorage.setItem("Message",JSON.stringify(oldMail))
+    }
+    else{
+        localStorage.setItem("Message",JSON.stringify(newMail))
+    }
+
+}
+
 $(document).on('click',':submit',function(e){
 
 })
